2337ce707a4cef3e0275a30fe3c7e18ab0d476ec
Saad Jareer <sjareer.c@stc.com.sa>
2024-07-10 08:55:59 +0300

    enable-authentication

--- HrApiDay11/src/sdaia-final-project-backend/java/org/example/helpers/AuthFilter.java
+++ HrApiDay11/src/sdaia-final-project-backend/java/org/example/helpers/AuthFilter.java
@@ -1,25 +1,25 @@
 package org.example.helpers;
 
-import jakarta.inject.Inject;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
-import org.example.dao.DepartmentDAO;
 import org.example.dto.ErrorMessage;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Base64;
+import java.util.List;
+import java.util.StringTokenizer;
 
 @Provider
 public class AuthFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        if(!requestContext.getUriInfo().getPath().contains("secures")) return;
+        if (!requestContext.getUriInfo().getPath().contains("secures")) return;
 
         List<String> authHeaders = requestContext.getHeaders().get("Authorization");
-        if (authHeaders != null && authHeaders.size() != 0) {
+        if (authHeaders != null && !authHeaders.isEmpty()) {
             String authHeader = authHeaders.get(0);
             authHeader = authHeader.replace("Basic ", "");
             authHeader = new String(Base64.getDecoder().decode(authHeader));
@@ -27,20 +27,31 @@ public class AuthFilter implements ContainerRequestFilter {
             String username = tokenizer.nextToken();
             String password = tokenizer.nextToken();
 
-            if (username.equals("admin") && password.equals("admin")) {
-                return;
+            if (!isUserAllowed(username, password)) {
+                ErrorMessage err = new ErrorMessage();
+                err.setErrorContent("LOGIN-ERROR: Invalid username or password");
+                err.setErrorCode(Response.Status.UNAUTHORIZED.getStatusCode());
+                Response res = Response.status(Response.Status.UNAUTHORIZED)
+                        .entity(err)
+                        .build();
+
+                requestContext.abortWith(res);
             }
         }
+    }
 
-        ErrorMessage err = new ErrorMessage();
-        err.setErrorContent("Please login");
-        err.setErrorCode(Response.Status.UNAUTHORIZED.getStatusCode());
-        err.setDocumentationUrl("https://google.com");
+    private boolean isUserAllowed(final String username, final String password) {
+        boolean isUserAllowed = false;
 
-        Response res = Response.status(Response.Status.UNAUTHORIZED)
-                .entity(err)
-                .build();
+        // Access the database and do this part yourself
+        // Step 1: Search the username in list of all users in your DB
+        // Step 2: If user exists, Get the user's password from database returned record and match with password in input parameter
+        // Step 3: If both passwords match [DB vs INPUT] then continue
+        // Step 4: Else return isUserAllowed [false]
 
-        requestContext.abortWith(res);
+        if (username.equals("admin") && password.equals("admin")) {
+            isUserAllowed = true;
+        }
+        return isUserAllowed;
     }
 }
