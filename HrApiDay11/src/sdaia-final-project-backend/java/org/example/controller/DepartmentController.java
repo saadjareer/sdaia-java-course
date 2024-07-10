0eb47db078dd82330680ab0045fcb258c58c6990
Saad Jareer <sjareer.c@stc.com.sa>
2024-07-10 08:55:41 +0300

    enable-cors

--- HrApiDay11/src/sdaia-final-project-backend/java/org/example/controller/DepartmentController.java
+++ HrApiDay11/src/sdaia-final-project-backend/java/org/example/controller/DepartmentController.java
@@ -52,7 +52,7 @@ public class DepartmentController {
             return Response
                     .ok(depts, MediaType.APPLICATION_JSON)
                     .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                    .header("Access-Control-Allow-Methods", "GET, POST, PUT")
                     .build();
 
         } catch (Exception e) {
